21e8d7aff77fafb02843068f1c8b3756f83b7a68
InChul <49394875+inchori@users.noreply.github.com>
2020-06-15 18:18:56 +0900

    delete unnecessary file

--- App.js
+++ App.js
@@ -5,22 +5,22 @@ import { NavigationContainer } from '@react-navigation/native';
 import MainScreen from "./components/MainScreen"
 import SignUp from './components/SignUp/SignUpPage'
 import SignInPage from './components/SignIn/SignInPage'
-import {View, Text} from 'react-native'
+import { View, Text } from 'react-native'
 import { render } from 'react-dom';
 import BottomBar from './components/MainScreen'
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import Navigator from './components/Navigator';
 import AppContainer from './PushAlarm/AppContainer'
 
-class App extends React.Component{
-  render(){
+class App extends React.Component {
+  render() {
     const isSignIn = false;
     return (
       <Provider store={store}>
-        <NavigationContainer> 
-            {/*{(isSignIn ? <SignUp/> : <SignInPage/>)}*/}
-            <Navigator/>
-            {/*<AppContainer/>*/}
+        <NavigationContainer>
+          {/*{(isSignIn ? <SignUp/> : <SignInPage/>)}*/}
+          <Navigator />
+          {/*<AppContainer/>*/}
         </NavigationContainer>
       </Provider>
     )
--- combinestore/CombineStoreReducers.js
+++
@@ -1,7 +0,0 @@
-import {combineReducers} from 'redux'
-import userReducer from '../user/reducers/UserReducer'
-import medicineReducer from '../store/reducers/MedicineReducer'
-
-const rootReducer = combineReducers({user : userReducer, medicine : medicineReducer})
-
-export default rootReducer
